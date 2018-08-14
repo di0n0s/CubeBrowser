7aa09d87b1130f99e9cf0f93194793687e59ad30
sfuentes <sfuentes@opentrends.net>
2018-08-14 20:28:13 +0200

    BaseFragment and Fragment extension created

---
+++ presentation/src/main/java/com/example/sfcar/cubebrowser/views/base/BaseFragment.kt
@@ -0,0 +1,22 @@
+package com.example.sfcar.cubebrowser.views.base
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+abstract class BaseFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? = inflater.inflate(layoutId(), container, false)
+
+    abstract fun layoutId(): Int
+
+}
+
