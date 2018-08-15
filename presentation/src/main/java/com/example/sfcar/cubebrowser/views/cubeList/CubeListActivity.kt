4193e2b4884aec03bc1cd6811836e316fecf10b2
sfuentes <sfuentes@opentrends.net>
2018-08-15 14:31:46 +0200

    CubeListActivity and Fragment created

---
+++ presentation/src/main/java/com/example/sfcar/cubebrowser/views/cubeList/CubeListActivity.kt
@@ -0,0 +1,32 @@
+package com.example.sfcar.cubebrowser.views.cubeList
+
+import android.os.Bundle
+import android.view.View
+import com.example.sfcar.cubebrowser.interfaces.CubeListActivityListener
+import com.example.sfcar.cubebrowser.views.base.BaseActivity
+import com.example.sfcar.cubebrowser.views.base.BaseFragment
+
+class CubeListActivity : BaseActivity(), CubeListActivityListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun setToolbarTitle() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun fragment(): BaseFragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setToolbarTitle(title: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun goToCubeActivity(url: String, view: View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
---
+++ presentation/src/main/java/com/example/sfcar/cubebrowser/views/cubeList/CubeListFragment.kt
@@ -0,0 +1,72 @@
+package com.example.sfcar.cubebrowser.views.cubeList
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.sfcar.cubebrowser.R
+import com.example.sfcar.cubebrowser.views.base.BaseFragment
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class CubeListFragment : BaseFragment(), CubeListView {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(layoutId(), container, false)
+    }
+
+    override fun layoutId(): Int = R.layout.fragment_cube_list
+
+    override fun setupFragmentComponent() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showProgressBar(show: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showRecyclerView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideRecyclerView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideEmptyView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showEmptyView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setItems() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setNullAdapter() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setRefreshingBehaviour() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setEmptyView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showErrorMessage(message: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
